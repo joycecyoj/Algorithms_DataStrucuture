bd4d8688315bca9ef68174d1ca91fdff6e6579d9
Joyce Chow <joycecyoj01@gmail.com>
2018-10-28 16:18:30 -0400

    solution-String_Search

--- REACTO/String_Search.js
+++ REACTO/String_Search.js
@@ -5,6 +5,21 @@
 // indexOf('howdy', 'hello world'); // should return -1
 // indexOf('oox', 'ooboxoooxo'); // should return 6
 
+
+// Time: O(n*m)
 function indexOf(needle, haystack) {
-  
+
+  for (let i = 0; i < haystack.length - needle.length; i++) {
+    for (let j = 0 ; j < needle.length; j++) {
+
+      if (needle[j] !== haystack[i + j]) {
+        break
+      }
+
+      if (j === needle.length - 1) {
+        return i
+      }
+    }
+  }
+  return -1
 }
