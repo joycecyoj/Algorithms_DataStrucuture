12e1e477b25dc331383158b01b7402d2bc96d76b
Joyce Chow <joycecyoj01@gmail.com>
2018-11-19 21:45:53 -0500

    solution-283.Move_Zeros

---
+++ LeetCode/283.Move_Zeros.js
@@ -0,0 +1,24 @@
+// Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Input: [0,1,0,3,12]; Output: [1,3,12,0,0]
+// Note:
+
+// You must do this in-place without making a copy of the array.
+// Minimize the total number of operations.
+
+// Time: O(n); Space: O(1)
+var moveZeroes = function(nums) {
+  let zeroCount = 0;
+
+  for (let i = 0; i < nums.length; i ++) {
+      if (nums[i] === 0) {
+          nums.splice(i, 1)
+          zeroCount ++
+          i --
+      }
+  }
+  while (zeroCount) {
+      nums.push(0)
+      zeroCount --
+  }
+};
